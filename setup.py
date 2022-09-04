fe69ef6856cc35cf4130efd657618f60f0441505
Comathon <Comathon2020@gmail.com>
2022-09-04 23:08:34 +0900

    modified setup.py file

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(
     version = '0.0.1',
     description = 'test',
     py_modules = ["myfunctions"],
+    packages = setuptools.find_packages(),
     package_dir = {'': 'comathon'}    
     )
