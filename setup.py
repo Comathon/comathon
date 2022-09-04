5d85d56f2631dce19d4a14cce67a9b605f61a65e
Comathon <Comathon2020@gmail.com>
2022-09-04 23:07:59 +0900

    Test commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = 'comathon',
+    version = '0.0.1',
+    description = 'test',
+    py_modules = ["myfunctions"],
+    package_dir = {'': 'comathon'}    
+    )
