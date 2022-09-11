a7c610cdbacea794fd3d18c062cf9cb0b147fa6f
Comathon <Comathon2020@gmail.com>
2022-09-11 14:34:44 +0900

    version 0.0.6

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='comathon',
-    version='0.0.5',
+    version='0.0.6',
     license='MIT',
     author="Comathon",
     author_email='Comathon2020@gmail.com',
     description = ("Comathon module for comathon.com website"),
-    packages=['src', 'tests'],
-    # packages=find_packages('src'),
+    packages=find_packages(),
     # package_dir={'': 'src'},
     url='https://github.com/Comathon/module',
     install_requires=['requests'],
