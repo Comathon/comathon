7e4d21e4e8691d36ad8fb4dd51763dcfa5caea9c
Comathon <Comathon2020@gmail.com>
2022-09-17 21:54:27 +0900

    test

--- comathon/test_restAPI.py
+++ comathon/test_restAPI.py
@@ -1,12 +1,14 @@
 import requests
 
-url = "http://121.137.95.97:8888/BotList"
-response = requests.get(url)
+print("Hello World")
+
+# url = "http://121.137.95.97:8888/BotList"
+# response = requests.get(url)
+# # print(response.json())
+
+
+# url = "http://121.137.95.97:8888/BotWithinUserList?botid=BOT002"
+# response = requests.get(url)
 # print(response.json())
-
-
-url = "http://121.137.95.97:8888/BotWithinUserList?botid=BOT002"
-response = requests.get(url)
-print(response.json())
 
 
