f26dff1cae655c20ddc2337c95ac77a247692890
Comathon <Comathon2020@gmail.com>
2022-09-22 23:52:52 +0900

    test

--- comathon/test_restAPI.py
+++ comathon/test_restAPI.py
@@ -2,13 +2,24 @@ import requests
 
 print("Hello World")
 
-# url = "http://121.137.95.97:8888/BotList"
-# response = requests.get(url)
-# # print(response.json())
+print("Get Bot List")
+
+url = "http://121.137.95.97:8889/BotList"
+response = requests.get(url)
+print(response.json())
+
+print("Call Bot 001")
+
+url = "http://121.137.95.97:8889/BotWithinUserList?botid=BOT001"
+response = requests.get(url)
+print(response.json())
 
 
-# url = "http://121.137.95.97:8888/BotWithinUserList?botid=BOT002"
-# response = requests.get(url)
-# print(response.json())
+print("Call Bot 002")
 
+url = "http://121.137.95.97:8889/BotWithinUserList?botid=BOT002"
+response = requests.get(url)
+print(response.json())
+
+print("End")
 
