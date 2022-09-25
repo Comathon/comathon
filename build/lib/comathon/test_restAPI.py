52b6843530f1e81de1adb1cc5e752f5ba9745dca
Comathon <Comathon2020@gmail.com>
2022-09-25 21:36:29 +0900

    version 0.0.8

--- build/lib/comathon/test_restAPI.py
+++ build/lib/comathon/test_restAPI.py
@@ -1,12 +1,25 @@
 import requests
 
-url = "http://121.137.95.97:8888/BotList"
-response = requests.get(url)
-# print(response.json())
+print("Hello World")
 
+print("Get Bot List")
 
-url = "http://121.137.95.97:8888/BotWithinUserList?botid=BOT002"
+url = "http://121.137.95.97:8889/BotList"
 response = requests.get(url)
 print(response.json())
 
+print("Call Bot 001")
+
+url = "http://121.137.95.97:8889/BotWithinUserList?botid=BOT001"
+response = requests.get(url)
+print(response.json())
+
+
+print("Call Bot 002")
+
+url = "http://121.137.95.97:8889/BotWithinUserList?botid=BOT002"
+response = requests.get(url)
+print(response.json())
+
+print("End")
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='comathon',
-    version='0.0.7',
+    version='0.0.8',
     license='MIT',
     author="Comathon",
     author_email='Comathon2020@gmail.com',
@@ -10,7 +10,7 @@ setup(
     packages=find_packages(),
     # package_dir={'': 'src'},
     url='https://github.com/Comathon/module',
-    install_requires=['requests'],
+    install_requires=['requests', 'pyupbit'],
     
 )
 
